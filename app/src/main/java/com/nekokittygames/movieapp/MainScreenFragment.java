0d289eafc5c95d973dde2d3f20b8b4287871e4ce
Katrina Swales <kat.swales@nekokittygames.com>
2015-08-27 17:02:44 +0100

    initial commit

---
+++ app/src/main/java/com/nekokittygames/movieapp/MainScreenFragment.java
@@ -0,0 +1,22 @@
+package com.nekokittygames.movieapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainScreenFragment extends Fragment {
+
+    public MainScreenFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_screen, container, false);
+    }
+}
